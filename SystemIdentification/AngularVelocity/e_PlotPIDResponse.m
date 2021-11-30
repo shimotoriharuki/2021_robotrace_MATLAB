b3d451f38dc61b533bc0d1014ede1e20745e6c8e
Haruki <underbird.h@gmail.com>
2021-11-30 18:51:36 +0900

    Msigの応答を記録するところが動かない

---
+++ SystemIdentification/AngularVelocity/e_PlotPIDResponse.m
@@ -0,0 +1,14 @@
+% PIDで制御した応答をプロット
+logs_origin = importdata("log/PIDRES.txt");
+number_of_data = 100;
+
+logs = logs_origin(1:number_of_data);
+x = 0:number_of_data-1;
+
+plot(x, logs);
+
+title('Msig response');
+
+
+
+
