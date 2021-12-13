eebb51e03fd8b5007a6ced6d69ed52448b7861b6
Haruki <underbird.h@gmail.com>
2021-12-13 18:11:11 +0900

    減速を実装

--- plotLogs/verificationDistanceCorrection.m
+++ plotLogs/verificationDistanceCorrection.m
@@ -1,10 +1,10 @@
 clf
 clear
-distance = load("datas/speed1_0vs1_3/DISTANCE.txt");
-theta = load("datas/speed1_0vs1_3/THETA.txt");
-vel_table = load("datas/speed1_0vs1_3/VELTABLE.TXT");
-distance2 = load("datas/speed1_0vs1_3/DISTANC2.txt");
-theta2 = load("datas/speed1_0vs1_3/THETA2.txt");
+distance = load("datas2/DISTANCE.txt");
+theta = load("datas2/THETA.txt");
+vel_table = load("datas2/VELTABLE.TXT");
+distance2 = load("datas2/DISTANC2.txt");
+theta2 = load("datas2/THETA2.txt");
 
 
 % データが有るところだけ抽出
@@ -14,22 +14,18 @@ theta = theta(1:size(distance)); %rad
 distance2 = nonzeros(distance2); %mm
 theta2 = theta2(1:size(distance2)); %rad
 
-pivot_lenght = 110; %mm
+
 x = 0;
 y = 0;
 th = 0;
 X = [];
 Y = [];
-X_sens = [];
-Y_sens = [];
 for i = 1:size(distance)
     x = x + distance(i) * cos(th + theta(i)/2);
     y = y + distance(i) * sin(th + theta(i)/2);
     th = th + theta(i);
     X = [X x];
     Y = [Y y];
-    X_sens = [X_sens, x + pivot_lenght * cos(th)];
-    Y_sens = [Y_sens, y + pivot_lenght * sin(th)];
 end
 
 x2 = 0;
@@ -37,23 +33,19 @@ y2 = 0;
 th2 = 0;
 X2 = [];
 Y2 = [];
-X_sens2 = [];
-Y_sens2 = [];
 for i = 1:size(distance2)
     x2 = x2 + distance2(i) * cos(th2 + theta2(i)/2);
     y2 = y2 + distance2(i) * sin(th2 + theta2(i)/2);
     th2 = th2 + theta2(i);
     X2 = [X2 x2];
     Y2 = [Y2 y2];
-    X_sens2 = [X_sens2, x2 + pivot_lenght * cos(th2)];
-    Y_sens2 = [Y_sens2, y2 + pivot_lenght * sin(th2)];
 end
 
 
 theta(theta==0) = 0.00001;
 radius = distance ./ theta;
-radius(radius>1500) = 1500;
-radius(radius<-1500) = -1500;
+radius(radius>5000) = 5000;
+radius(radius<-5000) = -5000;
 
 theta2(theta2==0) = 0.00001;
 radius2 = distance2 ./ theta2;
@@ -76,42 +68,55 @@ end
 
 figure(1)
 subplot(2, 1, 1)
-hold on
 scatter(X, Y, 10, colorAry)
 title('1走目')
-
-scatter(X_sens, Y_sens, 10)
-hold off
 axis equal
 
 subplot(2, 1, 2)
-hold on
-scatter(X2, Y2, 10, colorAry2)
-title('2走目')
-
-scatter(X_sens2, Y_sens2, 10)
-hold off
-axis equal
-
-% -------X_sens, Y_sensからdistanceとthetaを計算
-X_cor = []
-Y_cor = []
-for i = 1:size(distance)
-
-end
-
-figure(2)
-subplot(2, 1, 1)
 t = 0:radius_size-1;
 plot(t, abs(radius))
 title('半径')
 
-subplot(2, 1, 2)
-t = 0:radius_size2-1;
-plot(t, abs(radius2))
-title('半径')
-
-figure(3)
+figure(2)
 vel_table = vel_table(1:size(distance));
+subplot(2, 1, 1)
 t = 0:length(vel_table)-1;
 plot(t, vel_table)
+ylim([1.2 1.9])
+
+decelerate_table = vel_table;
+idx = length(vel_table);
+
+am = 0.1;
+while idx > 1
+    if decelerate_table(idx-1) - decelerate_table(idx) > 0 % 正のとき
+        t = 10e-3 / (decelerate_table(idx-1) - decelerate_table(idx));
+        a = (decelerate_table(idx-1) - decelerate_table(idx)) / t;
+        if a > am
+            decelerate_table(idx-1) = decelerate_table(idx) + am * t;
+        end
+    end
+    idx = idx - 1;
+end
+
+subplot(2, 1, 2)
+t = 0:length(decelerate_table)-1;
+plot(t, decelerate_table)
+ylim([1.2 1.9])
+
+%{
+%---------------------------------------%
+figure(3)
+first_distance = [0];
+for i = 1:size(distance)
+    first_distance = [first_distance, first_distance(i)+distance(i)];
+end
+
+t = 0:length(first_distance)-1;
+plot(t, first_distance)
+hold on
+
+t = 0:length(distance2)-1;
+plot(t, distance2)
+hold off
+%}
