fb7d55d2acb6b6a8d6f7b979846606dd9950c408
Haruki <underbird.h@gmail.com>
2022-11-04 17:50:51 +0900

    サイドラインとクロスラインを表示

--- plotLogs/plotLatestLogs.m
+++ plotLogs/plotLatestLogs.m
@@ -5,49 +5,88 @@ clf
 % ---データをロード---
 first_run_distances = load('workingDirectory/first_run_distances.txt');
 first_run_thetas = load('workingDirectory/first_run_thetas.txt');
+first_run_sideline_distances = load('workingDirectory/first_run_sideline_distances.txt');
+first_run_crossline_distances = load('workingDirectory/first_run_crossline_distances.txt');
+
+% second_run_distances = load('workingDirectory/second_run_distances.txt');
+% second_run_thetas = load('workingDirectory/second_run_thetas.txt');
 
 % --- データが有るところだけ抽出---
 first_run_distances = nonzeros(first_run_distances); %mm
 first_run_thetas = first_run_thetas(1 : size(first_run_distances)); %rad
-% first_run_thetas = first_run_thetas * 1.1;
+first_run_sideline_distances = nonzeros(first_run_sideline_distances); %mm
+first_run_crossline_distances = nonzeros(first_run_crossline_distances); %mm
+
+first_run_thetas = first_run_thetas * 0.96;
 
 
 % ---コースの形状をプロット---
-x = 0;
-y = 0;
-th = 0;
-X = zeros(size(first_run_distances));
-Y = zeros(size(first_run_distances));
-pre_theta = 0;
-for i = 1:size(first_run_distances)
-    x = x + first_run_distances(i) * cos(th + first_run_thetas(i)/2);
-    y = y + first_run_distances(i) * sin(th + first_run_thetas(i)/2);
 
-    th = th + first_run_thetas(i);
-    X(i) = x;
-    Y(i) = y;
-
-end
-
-% s = subplot(2, 1, 1);
-scatter(X, Y, 10)
+% subplot(2, 1, 1);
+plotCourseInformation(first_run_distances, first_run_thetas, first_run_sideline_distances, first_run_crossline_distances);
 title('1走目')
+
+
 axis equal
 
-function ax = plotCourseShape(distances, thetas)
+
+function plotCourseInformation(distances, thetas, sidelines, crosslines)
     x = 0;
     y = 0;
     th = 0;
-    X = zeros(size(distances));
-    Y = zeros(size(distances));
+    total_distance = 0;
+
+    positions = [zeros(size(distances)), zeros(size(distances))];
+    sideline_positions = [zeros(size(sidelines)), zeros(size(sidelines))];
+    crossline_positions = [zeros(size(crosslines)), zeros(size(crosslines))];
+    sideline_idx = 1;
+    crossline_idx = 1;
+
+    % コース情報をプロット
     for i = 1:size(distances)
+        % 位置を計算
         x = x + (distances(i)) * cos(th + thetas(i)/2);
         y = y + (distances(i)) * sin(th + thetas(i)/2);
     
         th = th + thetas(i);
-        X(i) = x;
-        Y(i) = y;
+        positions(i, :) = [x, y];
+
+        % サイドラインの位置を計算
+        if total_distance + 10 >= sidelines(sideline_idx) && sidelines(sideline_idx) >= total_distance - 10
+            y_sideline = y + 100 * cos(th);
+            sideline_positions(sideline_idx, :) = [x, y_sideline];
+            sideline_idx = sideline_idx + 1;
+
+            if sideline_idx > length(sidelines(:, 1))
+                sideline_idx = length(sidelines(:, 1));
+            end
+        end
+
+        % クロスラインの位置を計算
+        if total_distance + 10 >= crosslines(crossline_idx) && crosslines(crossline_idx) >= total_distance - 10
+            
+            crossline_positions(crossline_idx, :) = [x, y];
+            crossline_idx = crossline_idx + 1;
+
+            if crossline_idx > length(crosslines(:, 1))
+                crossline_idx = length(crosslines(:, 1));
+            end
+        end
+
+        total_distance = total_distance + distances(i);
     
     end
-    ax = subplot(2, 1, 1);
+    hold on
+    scatter(positions(:, 1), positions(:, 2)); % コースををプロット
+
+    crossline_nums = 1 : length(crossline_positions(:, 1));
+    scatter(crossline_positions(: ,1), crossline_positions(:, 2), 300, "red", "x", "LineWidth", 2); % クロスラインをプロット
+    text(crossline_positions(: ,1) + 50, crossline_positions(:, 2) + 50, string(crossline_nums), "Color", 'r');
+    
+    sideline_nums = 1 : length(sideline_positions(:, 1));
+    scatter(sideline_positions(: ,1), sideline_positions(:, 2), 300, "magenta", "*", "LineWidth", 2) % サイドラインをプロット
+    text(sideline_positions(: ,1) + 50, sideline_positions(:, 2) + 50, string(sideline_nums), "Color", "m");
+
+    hold off
 end
+ 
